3cebe031490dc22a84080587b5e7eff499c064cb
Enrique J. Ruiz <enriquejruizj@gmail.com>
2026-09-22 23:07:27 +0200

    ajuste en createServerClient

--- proxy.ts
+++ proxy.ts
@@ -9,7 +9,7 @@ export async function proxy(request: NextRequest) {
   // Creamos el cliente de Supabase optimizado para el middleware
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
